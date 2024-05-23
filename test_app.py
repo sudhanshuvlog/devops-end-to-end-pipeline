b99595989d4ee72c142ae31cc5caf621c081f748
aman <amangoswami@gmail.com>
2024-05-23 18:28:42 +0530

    process

--- test_app.py
+++ test_app.py
@@ -15,5 +15,5 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = quote('HelloGFG-18Bye')
+    expected_text = quote('HelloGFG')
     assert expected_text.encode() in response.data
