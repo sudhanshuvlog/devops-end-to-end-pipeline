74718220f0e1e1027c58a822c3c26c8657e30252
VIJAYSPARE <95615213+VIJAYSPARE@users.noreply.github.com>
2024-06-18 13:55:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def create_app():
 
     @app.route('/')
     def home():
-        return quote('Hurray devops 20')
+        return quote('Hello world  this is my first CICD PIPENINE')
 
     return app
 
